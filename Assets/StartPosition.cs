17ced3e2b1522dd9c88f33fda7aecb1fb1accf2d
Josh <pyreg1@gmail.com>
2017-07-06 20:06:59 -0400

    another derp :-)

---
+++ Assets/StartPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPosition : MonoBehaviour {
+	public Transform m_startPoint;
+	// Use this for initialization
+	void Start () {
+		m_startPoint = GameObject.FindGameObjectWithTag("StartPoint").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
